ce864c3925c59fab9bae0305267208b28441ced2
Artoras-code <vice.to.alarcon@gmail.com>
2025-10-04 18:00:56 -0300

    base del proyecto con el login base

---
+++ sistema_hospital/cuentas/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == "POST":
+   
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        # Autenticamos solo si hay datos
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")  # Redirige al dashboard si las credenciales son correctas
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+    
+    
+    return render(request, "cuentas/login.html")
