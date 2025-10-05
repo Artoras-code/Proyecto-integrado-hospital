d5c8a78e336cd79d76d1d69acd1a2f8b63be3601
Artoras-code <vice.to.alarcon@gmail.com>
2025-10-05 17:25:53 -0300

    Dashboard

--- sistema_hospital/sistema_hospital/urls.py
+++ sistema_hospital/sistema_hospital/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cuentas.urls'))
+    path('', include('cuentas.urls')),
+    path("dashboard/", include("dashboard.urls")),
 ]
