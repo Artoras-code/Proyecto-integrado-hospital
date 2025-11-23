5883450a2f4569095f1656da92928f465343d109
Artoras-code <vice.to.alarcon@gmail.com>
2025-11-23 18:06:04 -0300

    Preparando Frontend para despliegue en Vercel

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // Esta es la URL de tu backend de Django
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 });
 
-// Esto se ejecuta ANTES de cada petición.
+
+
 apiClient.interceptors.request.use(
   (config) => {
-    // 1. Obtenemos el token de localStorage
     const token = localStorage.getItem('accessToken');
-    
-    // 2. Si el token existe, lo añadimos a la cabecera (header)
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
