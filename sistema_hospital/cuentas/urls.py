d178449e157fac21182d0bb19d619f6c2cda4eb4
Artoras-code <vice.to.alarcon@gmail.com>
2025-11-23 21:58:18 -0300

    Arreglo error eso espero

--- sistema_hospital/cuentas/urls.py
+++ sistema_hospital/cuentas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import api_views 
+from . import api_views
 
 router = DefaultRouter()
+
 router.register(r'equipos', api_views.EquipoViewSet, basename='equipo')
 router.register(r'usuarios-opciones', api_views.UserOptionViewSet, basename='user-option')
 router.register(r'users', api_views.UserViewSet, basename='user')
@@ -14,6 +15,6 @@ urlpatterns = [
     path('api/auth/verify/', api_views.Verify2FAAPIView.as_view(), name='auth-verify'),
     path('api/auth/setup-2fa/', api_views.Generate2FAAPIView.as_view(), name='auth-setup-2fa'),
     path('api/auth/verify-setup/', api_views.VerifySetup2FAAPIView.as_view(), name='auth-verify-setup'),
+    path('api/auth/solicitar-clave/', api_views.SolicitarCambioClaveView.as_view(), name='auth-request-pass'),
     path('api/dashboard/stats/', api_views.dashboard_stats, name='dashboard-stats'),
-    path('api/auth/solicitar-clave/', api_views.SolicitarCambioClaveView.as_view(), name='auth-solicitar-clave'),
 ]
